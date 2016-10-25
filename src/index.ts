1dfcb2c99a49c59a093711a6de5de9951fafc4ed
Xavier Stouder <xavier@stouder.io>
2016-10-25 15:29:38 +0200

    Base electron app

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import {app, BrowserWindow} from "electron";
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({ width: 800, height: 600 });
+
+    win.loadURL(`file://${__dirname}/app/view/index.html`);
+
+    win.webContents.openDevTools();
+
+    win.on("closed", () => {
+        win = null;
+    });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+    process.platform !== "darwin" && app.quit(); // Code like if you were in Satan's church
+});
+
+app.on("activate", () => {
+    win === null &&  createWindow(); // Code like if you were in Satan's church
+});
