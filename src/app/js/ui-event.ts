8d766ebf2c9b5b4a7935cd4e5fa21f5247f99606
Luca Laissue <luca.laissue@idleweiss.ch>
2016-10-28 11:19:27 +0200

    Change pin button event

--- src/app/js/ui-event.ts
+++ src/app/js/ui-event.ts
@@ -1,7 +1,19 @@
 class UiEvent {
     private addFeedButton = document.querySelector("#add-feed-button");
+    private pinButton = document.querySelector("#pin-button");
 
     constructor(){
         this.addFeedButton.addEventListener("click", e => ModalManager.displayModal("#add-feed-modal"));
+
+        // Pin button
+        this.pinButton.addEventListener("click", e => {
+            const body = document.querySelector("body");
+            const classToCheck = "pinned";
+            if(body.classList.contains(classToCheck)) {
+                body.classList.remove(classToCheck);
+            } else {
+                body.classList.add(classToCheck);
+            }
+        });
     }
 }
