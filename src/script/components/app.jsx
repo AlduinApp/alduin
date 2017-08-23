fa9c35576160a11a926852ec073b56f6b58148d5
Xavier Stouder <xavier@stouder.io>
2017-08-23 10:35:31 +0200

    Persistence

--- src/script/components/app.jsx
+++ src/script/components/app.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import watch from 'redux-watch'
 
 import allReducers from '../reducers'
 
@@ -11,8 +12,10 @@ import MainContainer from './main-container'
 import AddFeedModal from './modals/add-feed-modal'
 import Loader from './loader'
 
+import Storage from '../utils/storage'
+
 class App extends React.Component {
-  render () {
+  render() {
     return (
       <div className='app'>
         <Header />
@@ -25,7 +28,18 @@ class App extends React.Component {
     )
   }
 }
-const store = createStore(allReducers)
+
+const localStorage = new Storage()
+
+const store = createStore(allReducers, localStorage.load())
+
+store.subscribe(watch(store.getState, 'FeedsReducer.feeds')(async datas => {
+  localStorage.requestStore({
+    FeedsReducer: {
+      feeds: datas
+    }
+  })
+}))
 
 ReactDOM.render(
   <Provider store={store}>
