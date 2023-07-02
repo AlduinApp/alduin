f7a1fe68ae8145786680546c250b89ea2550de9c
Xavier Stouder <xavier@stouder.io>
2023-07-02 18:07:50 +0200

    feat: remove feed capability

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,19 +1,36 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 
+import { OPEN_MODAL } from '../state/view/ViewActionType';
 import { ModalName } from '../state/view/ViewReducer';
 
 import useView from './useView';
+import useViewDispatch from './useViewDispatch';
 
 export default function useModal<T>(identifier: ModalName) {
   const view = useView();
+  const viewDispatch = useViewDispatch();
 
-  const { open, state } = useMemo(
+  const { open: isOpen, state } = useMemo(
     () => view.modals[identifier],
     [identifier, view.modals],
   );
 
+  const open = useCallback(
+    (state?: T) => {
+      viewDispatch({
+        type: OPEN_MODAL,
+        payload: {
+          identifier,
+          state: state ?? null,
+        },
+      });
+    },
+    [identifier, viewDispatch],
+  );
+
   return {
     open,
+    isOpen,
     state: state as T,
     isStateEmpty: state === null,
   };
