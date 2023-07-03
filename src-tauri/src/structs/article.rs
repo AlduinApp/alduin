c6afe5498047b062fb92ec7b5bbbae27521cada8
Xavier Stouder <xavier@stouder.io>
2023-07-03 23:51:46 +0200

    fix: handle missing content

--- src-tauri/src/structs/article.rs
+++ src-tauri/src/structs/article.rs
@@ -14,7 +14,13 @@ impl From<Entry> for Article {
     fn from(entry: Entry) -> Self {
         let id = entry.id;
         let title = entry.title.unwrap().content;
-        let content = entry.content.unwrap().body.unwrap();
+        let content = match entry.content {
+            Some(c) => c.body.unwrap(),
+            None => match entry.summary {
+                Some(s) => s.content,
+                None => String::from("No content found, please report this issue."),
+            },
+        };
         let date = entry.updated
             .unwrap_or_else(|| entry.published.unwrap_or_else(|| Utc::now()))
             .to_rfc3339();
